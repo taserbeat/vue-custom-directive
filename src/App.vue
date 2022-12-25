162af4976fa56eec9a8f733235dfe4aca60c6c02
taserbeat <taserbeat@gmail.com>
2022-12-25 16:18:10 +0900

    提供されたリソースを配置

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,22 @@
 <script setup lang="ts">
-import HelloWorld from "./components/HelloWorld.vue";
+import Form from "./components/Form.vue";
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <Form />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
+<style>
+* {
+  box-sizing: border-box;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+
+html {
+  font-family: sans-serif;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+body {
+  margin: 0;
+  background-color: #292929;
 }
 </style>
